7e1c27c74ee9a8f2195d44846adacd7f54bc04f7
xged <xged90@gmail.com>
2021-03-30 19:13:40 +0300

    STYLE 16

--- githubCommitCharCounter.py
+++ githubCommitCharCounter.py
@@ -14,7 +14,7 @@ RepoCounter = tuple[Counter['Char'], {'RepoUrl'}]  # tuple=SimpleNamespace
 def shell(s:str): return subprocess.run(s, capture_output=True, check=True, shell=True, text=True).stdout
 
 def getRepos(params={'q':'stars:>0'}, n=1000)->{'RepoUrl'}:  # !? stars:>=0 # noqa
-    assert n <= 1000
+    assert n<=1000
     perPage = min(100, n)
     npages = n//100 or 1
     urls = set()
@@ -35,7 +35,7 @@ def doCommits(repoUrls, inLastYears=5, commitCharMax=5000,
         with TemporaryDirectory() as fp:
             shell(f"git clone {repoUrl} {fp} --shallow-since={since} --shallow-submodules")
             commitHashes = shell(f"git -C {fp} log --pretty=format:%H").split()[:-1]
-            assert len(commitHashes) != 0
+            assert len(commitHashes)!=0
             for commitHash in commitHashes:
                 try:
                     diffPatch = shell(f"git -C {fp} diff {commitHash}~ {commitHash} --word-diff=porcelain --word-diff-regex=. --unified=0")
