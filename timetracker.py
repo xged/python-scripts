25a538a01308573613792c9efceff0175092c1a6
xged <xged90@gmail.com>
2021-04-10 12:40:40 +0300

    24

--- timetracker.py
+++ timetracker.py
@@ -16,9 +16,9 @@ def main():
         if data["tracking_start"] is None:
             data["tracking_start"] = currentTime
             print("Started Tracking..")
-            if data["work_hours"].setdefault(year, {}).setdefault(week, [0, 0, 0, 0, 0, 0, 0])[day-1]==0:
-                data["day_start"] = cp(currentTime)
-                data['timestamps'] = data['timestamps'][-20:]  #/
+        if data["work_hours"].setdefault(year, {}).setdefault(week, [0, 0, 0, 0, 0, 0, 0])[day-1]==0 and data["tracking_start"] is None:  # hack
+            data["day_start"] = cp(currentTime)
+            data['timestamps'] = data['timestamps'][-20:]
         else:
             currentWork = (currentTime-data["tracking_start"]).total_seconds()/3600
             data["work_hours"][year][week][day-1] += currentWork
